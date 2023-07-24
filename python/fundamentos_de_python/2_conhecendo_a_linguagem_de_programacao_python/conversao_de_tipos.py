c20636089b0c99203723cd03656e6eb29e3d3178
brunoloubraz <brunolou.braz@gmail.com>
2023-07-24 17:58:12 -0300

    modulo conhecendo a linguagem de programacao python finalizado

---
+++ python/fundamentos_de_python/2_conhecendo_a_linguagem_de_programacao_python/conversao_de_tipos.py
@@ -0,0 +1,34 @@
+# Inteiro pra float
+preco = 10
+print(preco)
+
+preco = float(preco)
+print(preco)
+
+preco = 10 / 5
+print(preco)
+
+# Float para inteiro
+preco = 10.30
+print(preco)
+
+preco = int(preco)
+print(preco)
+
+# Conversão por divisão
+
+preco = 10
+print(preco)
+print(preco/2)
+print(preco//2)
+
+# Númerico para string
+
+preco = 10.50
+idade = 28
+
+print(str(preco))
+print(str(idade))
+
+texto = f"idade {idade} preco {preco}"
+print(texto)

9f8b2ccf1335595fa0fa815b5422f5aaa0f2cc67
brunoloubraz <brunolou.braz@gmail.com>
2023-08-23 23:25:51 -0300

    conteudo em processo

---
+++ python/poo_python/conceitos_de_heranca/heranca_simples.py
@@ -0,0 +1,35 @@
+class Veiculo:
+    def __init__(self, cor, placa, numero_rodas):
+      self.cor = cor
+      self.placa = placa
+      self.numero_rodas = numero_rodas
+
+    def ligar_motor(self):
+        print('Ligando o motor')
+
+    def __str__(self):
+        return self.cor
+
+class Motocicleta(Veiculo):
+    pass
+
+class Carro(Veiculo):
+    pass
+
+class Caminhao(Veiculo):
+    def __init__(self, cor, placa, numero_rodas, carregado):
+        self.carregado = carregado
+
+    def esta_carregado(self):
+        print(f"{'Sim' if self.carregado else 'Não'} estou carregado")
+
+# moto = Motocicleta('preta', 'abc-1234',2)
+# print(moto)
+# moto.ligar_motor()
+
+# carro = Carro('branco', 'xde-0098', 4)
+# carro.ligar_motor()
+# # carro.esta_carregado()
+
+caminhao = Caminhao('roxo', 'gfd-8712', 8, True)
+caminhao.ligar_motor()
